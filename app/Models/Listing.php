077891eec7d048171dfd728256029d4797a1c02e
ayoub1elh <ay2oub49@gmail.com>
2024-10-10 15:33:07 +0100

    Refactor listing and update request classes + Create listing form and create function

--- app/Models/Listing.php
+++ app/Models/Listing.php
@@ -33,9 +33,20 @@ class Listing extends Model
     public function scopeFilter($query, array $filters)
     {
         if ($filters['search'] ?? false) {
-            $query
-            ->where('title', 'like', '%'. request('search') .'%')
-            ->orWhere('desc', 'like', '%'. request('search') .'%');
+            $query->where(function($q){
+                $q
+                ->where('title', 'like', '%'. request('search') .'%')
+                ->orWhere('desc', 'like', '%'. request('search') .'%');
+            });
+        }
+
+        if($filters['user_id'] ?? false) {
+            $query->where('user_id', request('user_id'));
+
+        }
+
+        if($filters['tag'] ?? false) {
+            $query->where('tags', 'like', '%'. request('tag') .'%');
         }
     }
 }
